e334e3f8ea4f76cb573d3da13d87f986e40f77ce
Ryan Eng <rye18625@gmail.com>
2025-02-03 22:47:26 -0500

    initial menu

---
+++ LucinaGame/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public void startGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+}
