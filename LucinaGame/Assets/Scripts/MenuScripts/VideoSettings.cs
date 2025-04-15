c837eaee62a1a2fbe763b950cd2f9a33c6a3a794
Ryan Eng <rye18625@gmail.com>
2025-04-15 16:48:43 -0400

    add sensitivity to main menu too

--- LucinaGame/Assets/Scripts/MenuScripts/VideoSettings.cs
+++ LucinaGame/Assets/Scripts/MenuScripts/VideoSettings.cs
@@ -1,15 +1,19 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class VideoSettings : MonoBehaviour
 {
     public TMP_Dropdown dropdown; 
+    public Slider sensitivitySlider;
 
     void Start()
     {
         // Set current dropdown selection
         dropdown.value = Screen.fullScreenMode == FullScreenMode.FullScreenWindow ? 0 : 1;
         dropdown.onValueChanged.AddListener(ChangeScreenMode);
+        sensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity", 2f);
+        sensitivitySlider.onValueChanged.AddListener(SetSens);
     }
 
     void ChangeScreenMode(int option)
@@ -23,5 +27,9 @@ public class VideoSettings : MonoBehaviour
             Screen.fullScreenMode = FullScreenMode.Windowed; // Windowed
         }
 
+    }
+
+    void SetSens(float sens) {
+        PlayerPrefs.SetFloat("Sensitivity", sens);
     }
 }
